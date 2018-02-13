e027eec5d7cbb48ac615e481e5a6d3df8d66ad09
Mårten Skogh <marten.skogh@gmail.com>
2018-02-13 16:45:55 +0100

    Added a viewer sript that I use to view some of the atoms and slabs

---
+++ HA2/viewer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from ase import Atoms
+from ase.build import fcc100, fcc111, add_adsorbate
+from ase.constraints import FixAtoms
+from ase.visualize import view
+
+slab = fcc100('Al',size=(2,2,2))
+carb_monox = Atoms('CO', positions=[(0,0,0),(0,0,1.13)])
+add_adsorbate(slab, carb_monox, 2, 'ontop')
+
+slab.center(vacuum=7.5, axis=2)
+
+view(slab)
